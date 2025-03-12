d7323ea03244e13b0bcc7c188724bd85e326f380
Gangadhar184 <zenistuig65@gmail.com>
2025-03-12 21:54:37 +0530

    type module

--- ai-model.js
+++ ai-model.js
@@ -1,20 +1,20 @@
 //initialize the model- Gemini SDK
+// require("dotenv").config();
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import "dotenv/config"
 
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-const genAI = new GoogleGenerativeAI("AIzaSyDCLZApat0J1hZw087e64JnIWtsSq8cRuY");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 //we have to write the function to generate text from AI
 async function generateTextFromModel(prompt) {
-        try{
-            const result = await model.generateContent([prompt]);
-            return result.response.text();
-        }
-        catch(error){
-            console.error("Error generating text: ",error);
-            return null;
-        }
+  try {
+    const result = await model.generateContent([prompt]);
+    return result.response.text();
+  } catch (error) {
+    console.error("Error generating text: ", error);
+    return null;
+  }
 }
-module.exports = { generateTextFromModel };
+export default { generateTextFromModel };
--- express.js
+++ express.js
@@ -1,8 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const {generateTextFromModel} = require("./ai-model");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import generateTextFromModel from "./ai-model.js";
 
 const app = express();
 
@@ -11,18 +10,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //post api to generate text
-app.post('/generate-text', async(req,res)=>{
-    const {prompt} = req.body;
-    try{
-        const generatedText = await generateTextFromModel(prompt);
-        res.json({text : generatedText});
-    }
-    catch(error){
-        console.log("Error in generating content: ", error);
-        res.status(500).json({error : "Failed to generate text"});
-    }
+app.post("/generate-text", async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    const generatedText = await generateTextFromModel(prompt);
+    res.json({ text: generatedText });
+  } catch (error) {
+    console.log("Error in generating content: ", error);
+    res.status(500).json({ error: "Failed to generate text" });
+  }
 });
 
-app.listen(3000, ()=>{
-    console.log("server running on port 3000");
-})
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
