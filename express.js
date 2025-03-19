8f9b39bf5b58ddca138d7707f771040b026ead12
Gangadhar184 <zenistuig65@gmail.com>
2025-03-19 19:58:45 +0530

    added

--- express.js
+++ express.js
@@ -27,6 +27,9 @@ app.post("/generate-text", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
